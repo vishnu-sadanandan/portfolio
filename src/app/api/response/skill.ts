e048230f6d05cab0c94bf92f5248b6753e3d25fc
Vishnu Sadanandan <47708994+vishnu-sadanandan@users.noreply.github.com>
2025-02-05 13:15:17 +0000

    Updated

--- src/app/api/response/skill.ts
+++ src/app/api/response/skill.ts
@@ -1,208 +1,147 @@
-import { PiSnowflakeLight } from "react-icons/pi";
-
 export const skill = [
     {
-        "id": 1,
+        "id": "react.js",
         "skill": "React.js",
-        "experience": "5 years",
-        "tag": ["Frontend", "JavaScript Framework"]
+        "experience": 2,
+        "tags": ["frontend", "frameworks"]
     },
     {
-        "id": 2,
-        "skill": "Next.js",
-        "experience": "3 years",
-        "tag": ["Frontend", "Full-Stack", "SSR"]
-    },
-    {
-        "id": 3,
-        "skill": "Node.js",
-        "experience": "5 years",
-        "tag": ["Backend", "JavaScript Runtime"]
-    },
-    {
-        "id": 4,
-        "skill": "Express.js",
-        "experience": "5 years",
-        "tag": ["Backend", "API Framework"]
-    },
-    {
-        "id": 5,
-        "skill": "Tailwind CSS",
-        "experience": "3 years",
-        "tag": ["Styling/UI", "CSS Framework"]
-    },
-    {
-        "id": 6,
-        "skill": "Material-UI",
-        "experience": "4 years",
-        "tag": ["Styling/UI", "Component Library"]
-    },
-    {
-        "id": 7,
-        "skill": "Carbon Design System",
-        "experience": "2 years",
-        "tag": ["Styling/UI", "Component Library"]
-    },
-    {
-        "id": 8,
-        "skill": "CDK Radial",
-        "experience": "2 years",
-        "tag": ["Styling/UI", "Component Library"]
-    },
-    {
-        "id": 9,
+        "id": "redux",
         "skill": "Redux",
-        "experience": "5 years",
-        "tag": ["State Management", "Frontend"]
+        "experience": 2,
+        "tags": ["frontend", "frameworks"]
     },
     {
-        "id": 10,
-        "skill": "REST API",
-        "experience": "5 years",
-        "tag": ["API", "Backend"]
+        "id": "node.js",
+        "skill": "Node.js",
+        "experience": 2,
+        "tags": ["backend", "frameworks"]
     },
     {
-        "id": 11,
-        "skill": "GraphQL",
-        "experience": "3 years",
-        "tag": ["API", "Backend"]
-    },
-    {
-        "id": 12,
-        "skill": "MongoDB",
-        "experience": "4 years",
-        "tag": ["NoSQL", "Database"]
-    },
-    {
-        "id": 13,
-        "skill": "MySQL",
-        "experience": "5 years",
-        "tag": ["SQL", "Database"]
-    },
-    {
-        "id": 14,
-        "skill": "Auth0",
-        "experience": "2 years",
-        "tag": ["Authentication", "Security"]
-    },
-    {
-        "id": 15,
-        "skill": "Vercel",
-        "experience": "3 years",
-        "tag": ["DevOps", "Hosting", "Frontend Deployment"]
-    },
-    {
-        "id": 16,
-        "skill": "Webpack",
-        "experience": "5 years",
-        "tag": ["Build Tool", "Frontend"]
-    },
-    {
-        "id": 17,
-        "skill": "Socket.IO",
-        "experience": "4 years",
-        "tag": ["Real-time Communication", "Backend"]
-    },
-    {
-        "id": 18,
-        "skill": "Git",
-        "experience": "5 years",
-        "tag": ["Version Control", "Collaboration"]
-    },
-    {
-        "id": 19,
-        "skill": "Bitbucket",
-        "experience": "5 years",
-        "tag": ["Version Control", "Collaboration"]
-    },
-    {
-        "id": 20,
-        "skill": "JIRA",
-        "experience": "5 years",
-        "tag": ["Project Management", "Agile"]
-    },
-    {
-        "id": 21,
-        "skill": "Jenkins",
-        "experience": "4 years",
-        "tag": ["CI/CD", "DevOps"]
-    },
-    {
-        "id": 22,
-        "skill": "PHP",
-        "experience": "5 years",
-        "tag": ["Backend", "Programming Language"]
-    },
-    {
-        "id": 23,
-        "skill": "HTML/CSS",
-        "experience": "9 years",
-        "tag": ["Frontend", "Web Development"]
-    },
-    {
-        "id": 24,
-        "skill": "JavaScript",
-        "experience": "9 years",
-        "tag": ["Programming Language", "Frontend", "Backend"]
-    },
-    {
-        "id": 25,
-        "skill": "TypeScript",
-        "experience": "4 years",
-        "tag": ["Programming Language", "Frontend", "Backend"]
-    },
-    {
-        "id": 26,
-        "skill": "Looker",
-        "experience": "2 years",
-        "tag": ["Data Visualization", "BI Tool"]
-    },
-    {
-        "id": 27,
-        "skill": "npm",
-        "experience": "5 years",
-        "tag": ["Package Manager", "JavaScript"]
-    },
-    {
-        "id": 28,
-        "skill": "Mapbox GL",
-        "experience": "2 years",
-        "tag": ["Mapping", "Geospatial"]
-    },
-    {
-        "id": 29,
-        "skill": "Mediasoup",
-        "experience": "2 years",
-        "tag": ["WebRTC", "Real-time Communication"]
-    },
-    {
-        "id": 30,
-        "skill": "D3.js",
-        "experience": "3 years",
-        "tag": ["Data Visualization", "JavaScript Library"]
-    },
-    {
-        "id": 31,
-        "skill": "Chart.js",
-        "experience": "3 years",
-        "tag": ["Data Visualization", "JavaScript Library"]
-    },
-    {
-        "id": 32,
-        "skill": "WebRTC",
-        "experience": "3 years",
-        "tag": ["Real-time Communication", "Networking"]
-    },
-    {
-        "id": 33,
+        "id": "microservice architecture",
         "skill": "Microservice Architecture",
-        "experience": "3 years",
-        "tag": ["Frontend"]
+        "experience": 2,
+        "tags": ["backend", "architecture"]
     },
     {
-        "id": 34,
-        "skill": "Snowflake",
-        "experience": "3 years",
-        "tag": ["Database"]
+        "id": "looker",
+        "skill": "Looker",
+        "experience": 2,
+        "tags": ["backend"]
     },
+    {
+        "id": "oauth",
+        "skill": "OAuth",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "snowflake",
+        "skill": "Snowflake",
+        "experience": 2,
+        "tags": ["database"]
+    },
+    {
+        "id": "chartjs",
+        "skill": "ChartJs",
+        "experience": 2,
+        "tags": ["frontend"]
+    },
+    {
+        "id": "dsa",
+        "skill": "DSA",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "css flex",
+        "skill": "CSS flex",
+        "experience": 2,
+        "tags": ["frontend"]
+    },
+    {
+        "id": "cdk design system",
+        "skill": "CDK Design System",
+        "experience": 2,
+        "tags": ["frontend", "frameworks"]
+    },
+    {
+        "id": "ibm watson discovery api",
+        "skill": "IBM Watson Discovery API",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "material-ui",
+        "skill": "Material-UI",
+        "experience": 2,
+        "tags": ["frontend", "frameworks"]
+    },
+    {
+        "id": "css",
+        "skill": "CSS",
+        "experience": 2,
+        "tags": ["frontend"]
+    },
+    {
+        "id": "chart.js",
+        "skill": "Chart.js",
+        "experience": 2,
+        "tags": ["frontend"]
+    },
+    {
+        "id": "mapbox gl",
+        "skill": "Mapbox GL",
+        "experience": 2,
+        "tags": ["frontend"]
+    },
+    {
+        "id": "javascript",
+        "skill": "JavaScript",
+        "experience": 2,
+        "tags": ["frontend", "backend"]
+    },
+    {
+        "id": "websocket",
+        "skill": "Websocket",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "mediasoup",
+        "skill": "Mediasoup",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "webrtc",
+        "skill": "WebRTC",
+        "experience": 2,
+        "tags": ["frontend", "backend"]
+    },
+    {
+        "id": "php",
+        "skill": "PHP",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "mysql",
+        "skill": "MySQL",
+        "experience": 2,
+        "tags": ["database", "sql"]
+    },
+    {
+        "id": "saml",
+        "skill": "Saml",
+        "experience": 2,
+        "tags": ["backend"]
+    },
+    {
+        "id": "html",
+        "skill": "HTML",
+        "experience": 2,
+        "tags": ["frontend"]
+    }
 ]
+
--- src/app/components/Cards/details.tsx
+++ src/app/components/Cards/details.tsx
@@ -7,6 +7,7 @@ import IconButton from "../Button/IconButton";
 import { FiChevronDown, FiCpu, FiGithub, FiLink } from "react-icons/fi";
 import { Role, Skill } from "@/app/api/response/types";
 import Image from "next/image";
+import { Tag } from "../Tags";
 interface PageViewProps {
   page: string;
   menuOptions?: Skill[];
@@ -25,7 +26,7 @@ const PageView: React.FC<PageViewProps> = ({
   currentRole,
 }) => {
   return (
-    <div className="flex flex-col w-full h-screen rounded overflow-hidden shadow-lg bg-white transition-shadow duration-300 hover:shadow-2xl">
+    <div className="flex flex-col w-full min-h-screen rounded overflow-hidden shadow-lg bg-white transition-shadow duration-300 hover:shadow-2xl">
       <div className="p-4">
         <div className="flex col-2 justify-between items-center">
           <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>
@@ -43,32 +44,45 @@ const PageView: React.FC<PageViewProps> = ({
         <div className="flex col-2 justify-between items-center">
           <h4 className="text-2xl font-semibold text-gray-800">{`Roles and Responsibilities`}</h4>
         </div>
-        <p className="text-gray-600 mt-2">
-          <div
-            className="mt-2 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-lg rounded-xl z-50 
+        <div
+          key="idd"
+          className="mt-2 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-lg rounded-xl z-50 
                      h-48 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-800"
-          >
-            {currentRole?.map((role: Role) => (
-              <>
-                <div className="bg-white dark:bg-gray-800 px-4 py-2 font-semibold border-b border-gray-300 dark:border-gray-700">
-                  {role.roleName}
-                </div>
+        >
+          {currentRole?.map((role: Role) => (
+            <div key={role.id}>
+              <div className="bg-white dark:bg-gray-800 px-4 py-2 font-semibold border-b border-gray-300 dark:border-gray-700">
+                {role.roleName}
+              </div>
 
-                <ul className="py-2">
-                  {currentRole?.map((role: Role) => (
-                    <li
-                      key={role.roleName}
-                      className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer"
-                    >
-                      {role.roleDescription}
-                    </li>
-                  ))}
-                </ul>
-              </>
-            ))}
-          </div>
-        </p>
+              <ul className="py-2">
+                {currentRole?.map((role: Role) => (
+                  <li
+                    key={role.roleName}
+                    className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer"
+                  >
+                    {role.roleDescription}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
+      <div className="p-4">
+        <div className="flex col-2 justify-between items-center">
+          <h4 className="text-2xl font-semibold text-gray-800">{`Technologies Used`}</h4>
+        </div>
+        <div
+          className="mt-2 p-2 h-28 w-100 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 shadow-lg rounded-xl z-50 
+                     h-48 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-800"
+        >
+          {menuOptions?.map((skill: Skill) => (
+            <Tag key={skill.id} text={skill.skill} />
+          ))}
+        </div>
+      </div>
+
       <div className="p-4 bg-gray-100">
         <div className="flex space-x-4">
           <IconButton Icon={FiLink} tooltip="Hosted URL" />
